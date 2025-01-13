65f9f91d1e364554ed53f78a523eb9c7c4d582f1
sfen99 <fenske@tutanota.com>
2025-01-14 00:15:18 +0100

    first commit

---
+++ lib/quote.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'quote.dart';
+import 'quote_card.dart';
+
+class Quote {
+
+  String text;
+  String author;
+
+  Quote({ required this.text, required this.author });
+
+}
+
+//Quote myquote = Quote(text: 'this is the quote text', author: 'oscar wilde');
